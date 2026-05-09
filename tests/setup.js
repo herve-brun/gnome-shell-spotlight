ebcdcb8fa14d6eb310c413b621c9f7c843336f1a
Hervé Brun <herve-brun@users.noreply.github.com>
2026-05-09 17:45:40 +0200

    Fix Jest mock hoisting violation by moving require inside factory functions

--- tests/setup.js
+++ tests/setup.js
@@ -1,32 +1,36 @@
 // Mock des modules GNOME pour Jest
-const { Gio, Clutter, Cogl, GLib, global: globalMock } = require('./__mocks__/gnome.js');
-
-// Mock de 'gi' (GObject Introspection)
-jest.mock('gi', () => ({
-  Clutter,
-  Cogl,
-  Gio,
-  GLib,
-}));
-
-// Mock de 'imports' (pour les modules GNOME Shell)
-jest.mock('imports', () => ({
-  gi: {
+jest.mock('gi', () => {
+  const { Gio, Clutter, Cogl, GLib } = require('./__mocks__/gnome.js');
+  return {
     Clutter,
     Cogl,
     Gio,
     GLib,
-  },
-  misc: {
-    extensionUtils: {
-      getCurrentExtension: () => ({
-        path: '/path/to/extension',
-      }),
+  };
+});
+
+// Mock de 'imports' (pour les modules GNOME Shell)
+jest.mock('imports', () => {
+  const { Gio, Clutter, Cogl, GLib, global: globalMock } = require('./__mocks__/gnome.js');
+  return {
+    gi: {
+      Clutter,
+      Cogl,
+      Gio,
+      GLib,
     },
-  },
-}));
+    misc: {
+      extensionUtils: {
+        getCurrentExtension: () => ({
+          path: '/path/to/extension',
+        }),
+      },
+    },
+  };
+});
 
 // Configure global pour les tests
+const { Gio, Clutter, Cogl, GLib, global: globalMock } = require('./__mocks__/gnome.js');
 global.global = globalMock;
 global.Clutter = Clutter;
 global.Cogl = Cogl;
